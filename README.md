4bed199474ec5c4bbb8ae8d113f25509c14f5df8
Renee Messick <rnmessick@gmail.com>
2019-09-18 11:59:10 -0700

    this was super crazy

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # task-master-lambda
+
+## Java Insert/Update Functionality
+- Create a lambda function, in Java, that can add a record to your Taskmaster table.
+Run this only in “Test” mode
+
+- It should receive the same object that your API was handling earlier.
+
+- Repeat for “PUT” / update functionality
+
+## credits and contributions
+- Matt Stuhring
+- Nhu Trinh
+- @Bomibear
+- Travis Cox
+- Peter Lee
+- Padmapriya Ganapathi
+- Renee Messick
+- Jack Kinne
+- Marisha Hoza,
+- Melfi Perez
+- Jane Hur
+- Trevor Dobson
---
+++ src/main/java/lambda/dynamo/HistoryObj.java
@@ -0,0 +1,38 @@
+package lambda.dynamo;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+
+@DynamoDBDocument
+public class HistoryObj {
+    String time;
+    String action;
+
+    public HistoryObj(){}
+
+    public HistoryObj(String time, String action){
+        this.time = time;
+        this.action = action;
+    }
+
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+
+    public String toString() {
+        return String.format("%s @ %s", this.getAction(), this.getTime());
+    }
+}
